c053e74f90229375a3039243c95d480c5978a06a
Sascha Deeg <sascha.deeg@gmail.com>
2019-06-12 20:09:38 +0200

    Fehlerhafte Schreibweise korregiert

---
+++ src/main/java/de/youthclubstage/infrastructure/nginxdnsreg/NginxDnsRegApplication.java
@@ -0,0 +1,14 @@
+package de.youthclubstage.infrastructure.nginxdnsreg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NginxDnsRegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NginxDnsRegApplication.class, args);
+	}
+
+
+}
--- src/test/java/de/youthclubstage/infrastructure/nginxdnsreg/NginxDnsRegApplicationTests.java
+++ src/test/java/de/youthclubstage/infrastructure/nginxdnsreg/NginxDnsRegApplicationTests.java
@@ -1,14 +1,13 @@
-package de.youthclubstage.infrastructure.ngnixdnsreg;
+package de.youthclubstage.infrastructure.nginxdnsreg;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class NgnixDnsRegApplicationTests {
+public class nginxDnsRegApplicationTests {
 
 	@Test
 	public void contextLoads() {
