eef1824c0267cb867c259a0649dbd30019c810e8
Sascha Deeg <sascha.deeg@gmail.com>
2019-06-12 20:32:22 +0200

    Fehlerhafte Schreibweise korregiert

--- src/main/java/de/youthclubstage/infrastructure/nginxdnsreg/service/DnsService.java
+++ src/main/java/de/youthclubstage/infrastructure/nginxdnsreg/service/DnsService.java
@@ -57,10 +57,9 @@ public class DnsService {
         if(activeProfile.equalsIgnoreCase("docker")) {
             List<DnsDto> all = getAll();
             for (DnsDto dnsDto : all) {
-                System.out.println(templateService);
-                System.out.println(dnsDto.getSource());
-                System.out.println(dnsDto.getTarget());
-                templateService.writeTemplate(dnsDto.getSource(), dnsDto.getTarget());
+               if(dnsDto != null) {
+                   templateService.writeTemplate(dnsDto.getSource(), dnsDto.getTarget());
+               }
             }
             reloadnginx();
         }
