87b0bc0ea8b946bff4a3883a42e4e9a7fe87d1eb
Sascha Deeg <sascha.deeg@gmail.com>
2019-06-12 20:41:46 +0200

    Start Script angepasst

--- docker-scripts/start.sh
+++ docker-scripts/start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 rm  -f /etc/nginx/conf.d/*
-java -Dspring.profiles.active=docker -jar /etc/nginx-config/app/nginx-dns-reg.jar && nginx -g daemon off;
+java -Dspring.profiles.active=docker -jar /etc/nginx-config/app/nginx-dns-reg.jar & nginx -g daemon off && fg;
+
