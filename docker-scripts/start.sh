6b156d493fceb65fb0934b02f8ffc5b63da1efbb
Sascha Deeg <sascha.deeg@gmail.com>
2019-06-13 00:00:52 +0200

    Rückgänig da fuer das Reload Root Rechte benötigt werden :(

--- docker-scripts/start.sh
+++ docker-scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm  -f /etc/nginx/conf.d/*
-su - www-data -s /bin/bash -c 'java -Dspring.profiles.active=docker -jar /etc/nginx-config/app/nginx-dns-reg.jar' &
+java -Dspring.profiles.active=docker -jar /etc/nginx-config/app/nginx-dns-reg.jar &
 nginx -g 'daemon off;'
 
