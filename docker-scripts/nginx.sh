bcf8ddb4a7e8c8ad751dab49bb9b62c4eea58e90
Sascha Deeg <sascha.deeg@gmail.com>
2019-06-12 21:15:10 +0200

    Anpassung weil Nginx nicht startet

--- docker-scripts/nginx.sh
+++ docker-scripts/nginx.sh
@@ -3,4 +3,5 @@ apt-get update
 apt-get upgrade -y
 apt-get install nginx -y
 echo "" > /var/run/nginx.pid
-chown www-data:www-data /var/run/nginx.pid
+chown nginx:nginx /var/run/nginx.pid
+chown -R nginx:nginx /etc/nginx/conf.d/
