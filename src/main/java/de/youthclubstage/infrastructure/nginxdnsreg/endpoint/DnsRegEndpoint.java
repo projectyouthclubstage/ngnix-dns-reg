94bdf1e51c1943430756bd921d898d1c24114cef
Sascha Deeg <sascha.deeg@gmail.com>
2019-06-21 14:45:01 +0200

    Debug Info

--- src/main/java/de/youthclubstage/infrastructure/nginxdnsreg/endpoint/DnsRegEndpoint.java
+++ src/main/java/de/youthclubstage/infrastructure/nginxdnsreg/endpoint/DnsRegEndpoint.java
@@ -4,6 +4,8 @@ package de.youthclubstage.infrastructure.nginxdnsreg.endpoint;
 import de.youthclubstage.infrastructure.nginxdnsreg.endpoint.model.DnsCreateUpdateDto;
 import de.youthclubstage.infrastructure.nginxdnsreg.endpoint.model.DnsDto;
 import de.youthclubstage.infrastructure.nginxdnsreg.service.DnsService;
+import org.apache.logging.log4j.LogManager;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +20,8 @@ public class DnsRegEndpoint {
 
     private final DnsService dnsService;
 
+    private static Logger log = LogManager.getLogger(DnsRegEndpoint.class)
+
     @Autowired
     public DnsRegEndpoint(DnsService dnsService){
         this.dnsService = dnsService;
@@ -30,6 +34,7 @@ public class DnsRegEndpoint {
 
     @PostMapping( value = "dns", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
     ResponseEntity<DnsDto> create(@RequestBody DnsCreateUpdateDto createUpdateDto){
+        log.error(createUpdateDto.toString());
         DnsDto dnsDto = dnsService.createDns(createUpdateDto);
         return ResponseEntity.ok(dnsDto);
     }
